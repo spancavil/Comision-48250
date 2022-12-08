256e54d7e3a89246a0bc03293fec931ae6a40ae0
spancavil <soancavil@gmail.com>
2022-12-07 22:36:36 -0300

    Desarrollo clase 2

---
+++ Clase 2 - Instalación y configuración del entorno/Curso React JS/Proyecto/clase2/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Comisión 48250. We love carts
+        </p>
+        <a
+          className="App-link"
+          href="https://www.coderhouse.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Coderhouse
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- Clase 2 - Instalación y configuración del entorno/spreadDestructuring.js
+++ Clase 2 - Instalación y configuración del entorno/spreadDestructuring.js
@@ -1,5 +1,41 @@
 //Destructuring de objetos
+const animal = {
+    patas: 4,
+    maulla: true,
+    visionNocturna: true
+}
+
+const bicho = {
+
+}
+
+console.log(animal.maulla);
+
+//Acceder a la propiedad a través de una variable
+const {maulla} = animal
+
+// console.log(accion);
 
 //Destructuring de arrays
 
+const frutas = ['pera', 'banana', 'frutilla', 'maracuya', 'kiwi', 'mango']
+
+// console.log(frutas[2])
+
+const [fruta1, fruta2, fruta3, ...restoFrutas] = frutas
+
+console.log(fruta1);
+console.log(restoFrutas);
+
 //Spread operator (...) para objetos y arrays
+const animalSuperpoderoso = {
+    ...animal,
+    vuela: true,
+    visionRayosLaser: true,
+}
+
+// console.log(animalSuperpoderoso);
+
+const frutasExtended = [...frutas, 'cereza', 'durazno']
+
+console.log(frutasExtended);
