11a18ad41ddb28da78e27bc173a1ca23a410b243
spancavil <soancavil@gmail.com>
2022-12-19 22:34:03 -0300

    Desarrollo clase 5

--- Clase 5 - Componentes II/clase5/src/components/PartidoCount/index.jsx
+++ Clase 5 - Componentes II/clase5/src/components/PartidoCount/index.jsx
@@ -1,20 +1,65 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './styles.scss'
 
 const PartidoCount = () => {
+ 
+  const [argCount, setArgCount] = useState(0)
+  const [frCount, setFrCount] = useState(0)
+  const [block, setBlock] = useState(false)
+
+  
+  //Si quiero que el callback se ejecute cuando se monta el componente, el array de dependencias []
+  useEffect(()=> {
+    console.log("Se montó el componente")
+    setTimeout(()=> {
+      setBlock(true)
+      alert("Termino el partido")
+    }, 10000)
+  }, [])
+
+  useEffect(()=> {
+    if (argCount > 0) {
+      alert("Gol de Argentina, caramba!")
+    }
+  }, [argCount])
+
+  useEffect(()=> {
+    console.log("Francia metió un gol (de penal seguro)")
+  }, [frCount])
+  
+  const onArgGoal = () => {
+    if (!block) {
+      setArgCount(argCount + 1)
+    }
+  }
+
+  const onFrGoal = () => {
+    if (!block) {
+      setFrCount(frCount + 1)
+    }
+  }
+
+  const onReset = () => {
+    if (block) {
+      setArgCount(0)
+      setFrCount(0)
+    }
+  }
+
   return (
     <div className='match-container'>
         <div className='result-container'>
             <img src='/Assets/argFlag.jpg' alt="arg" style={{width: 60}}/>
             <span>Argentina</span>
-            <h3>0</h3>
-            <h3>0</h3>
+            <h3>{argCount}</h3>
+            <h3>{frCount}</h3>
             <span>France</span>
             <img src='/Assets/franceFlag.jpg' alt="fr" style={{width: 60}}/>
         </div>
         <div className='button-container'>
-            <button className='button-count'>Gol de Argentina</button>
-            <button className='button-count'>Gol de Francia</button>
+            <button className='button-count' onClick={onArgGoal}>Gol de Argentina</button>
+            <button className='button-count' onClick={onFrGoal}>Gol de Francia</button>
+            <button className='button-count' onClick={onReset}>Reiniciar</button>
         </div>
     </div>
   )
